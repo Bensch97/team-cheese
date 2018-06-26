6e164b911a4c1c4f3503b8e02b68143acebff512
Jake Berg <jacobvictorberg@gmail.com>
2018-06-26 11:37:43 -0400

    fixed package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import DataCall from './components/DataCall.jsx'
 import {Route, Switch} from 'react-router'
 import './App.css';
 import Homepage from './components/Homepage.jsx';
@@ -16,7 +15,6 @@ class App extends Component {
               <Route exact path='/' render={() => <Homepage></Homepage>}/>
               <Route path='/group' render={() => <Grouppage></Grouppage>}/>
               <Route path='/login' render={() => <Loginpage></Loginpage>}/>
-              <DataCall />
             </Switch>
         </React.Fragment>
       </div>
