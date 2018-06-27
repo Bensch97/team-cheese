23f5abc4f8634a0c00acee8a6b4dc38dbae6211a
Bensch97 <ben.w.schacht@gmail.com>
2018-06-27 17:17:48 -0400

    Login page

---
+++ src/reducers/reducer.jsx
@@ -0,0 +1,16 @@
+
+import { REGISTER, LOGIN } from "../actions";
+import * as R from 'ramda';
+const initialState= [];
+
+const foodFetchReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case REGISTER:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
+export default foodFetchReducer
