1979459c02c0b5440817b46e449cc429106c4d58
Bensch97 <ben.w.schacht@gmail.com>
2018-07-03 10:17:42 -0400

    groupwork

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,6 +7,7 @@ import DataCall from './DataCall';
 import Userhome from './Userhome';
 import Calendar from './Calendar';
 import DonorList from './DonorList';
+import GroupRegister from './GroupRegister';
 
 class Navigation extends Component {
 
@@ -17,6 +18,7 @@ class Navigation extends Component {
                 <Sidebar className="color" as={Menu} animation='overlay' icon='labeled' inverted vertical visible width='thin'>
                     <Link to="/navigation/home"><Menu.Item as='a'><Icon name='home' />Home</Menu.Item></Link>
                     <Link to="/navigation/group"><Menu.Item as='a'><Icon name='group' />Groups</Menu.Item></Link>
+                    <Link to='/navigation/groupregister'><Menu.Item as='a'><Icon name='plus' />Register A Group</Menu.Item></Link>
                     <Link to="/navigation/calendar"><Menu.Item as='a'><Icon name='calendar alternate outline' />Calendar</Menu.Item></Link>
                     <Link to='/navigation/donorlist'><Menu.Item as='a'><Icon name='caret square down outline' />Donor List</Menu.Item></Link>
                 </Sidebar>
@@ -25,6 +27,7 @@ class Navigation extends Component {
                     <Segment basic>
                         <Switch>
                             <Route path='/navigation/group' render={() => <Grouppage />} />
+                            <Route path='/navigation/groupregister' render={() => <GroupRegister />} />
                             <Route path='/navigation/home' render={() => <Userhome />} />
                             <Route path='/navigation/calendar' render={() => <Calendar />} />
                             <Route path='/navigation/data' render={() => <DataCall />} />
